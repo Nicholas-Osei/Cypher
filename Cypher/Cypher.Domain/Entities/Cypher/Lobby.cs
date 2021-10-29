2b65d1cf25226388b9996aeeb54a274d5a1a67cc
yvesdj <yves.dejonghe@student.ap.be>
2021-10-29 11:03:44 +0200

    Added entities to Domain project, Needs migration and updated DbContext

---
+++ Cypher/Cypher.Domain/Entities/Cypher/Lobby.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCoreHero.Abstractions.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cypher.Domain.Entities.Cypher
+{
+    public class Lobby : AuditableEntity
+    {
+        public string Name { get; set; }
+        public Player LobbyAdmin { get; set; }
+        public virtual ICollection<PlayerLobby> PlayerLobbies { get; set; }
+    }
+}
