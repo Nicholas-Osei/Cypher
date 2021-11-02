8edaf7725bab3078d1408c061d18c4fb11b3bed0
yvesdj <yves.dejonghe@student.ap.be>
2021-11-03 00:01:56 +0100

    Added PlayerController, Started CQRS pattern: Added get all query

---
+++ Cypher/Cypher.Api/Controllers/v1/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using Cypher.API.Controllers;
+using Cypher.Application.Features.Players.Queries.GetAllPaged;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cypher.Api.Controllers.v1
+{
+    public class PlayerController : BaseApiController<PlayerController>
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int pageNumber, int pageSize)
+        {
+            var players = await _mediator.Send(new GetAllPlayersQuery(pageNumber, pageSize));
+            return Ok(players);
+        }
+    }
+}
---
+++ Cypher/Cypher.Application/Features/Players/Queries/GetAllPaged/GetAllPlayersQuery.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreHero.Results;
+using Cypher.Application.Extensions;
+using Cypher.Application.Interfaces.Repositories;
+using Cypher.Domain.Entities.Cypher;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cypher.Application.Features.Players.Queries.GetAllPaged
+{
+    public class GetAllPlayersQuery : IRequest<PaginatedResult<GetAllPlayersResponse>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public GetAllPlayersQuery(int pageNr, int pageSize)
+        {
+            PageNumber = pageNr;
+            PageSize = pageSize;
+        }
+
+        public class GetAllPlayersQueryHandler : IRequestHandler<GetAllPlayersQuery, PaginatedResult<GetAllPlayersResponse>>
+        {
+            private readonly IPlayerRepository _repo;
+            public GetAllPlayersQueryHandler(IPlayerRepository repository)
+            {
+                _repo = repository;
+            }
+
+            public async Task<PaginatedResult<GetAllPlayersResponse>> Handle(GetAllPlayersQuery request, CancellationToken cancellationToken)
+            {
+                Expression<Func<Player, GetAllPlayersResponse>> expression = e => new GetAllPlayersResponse
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    IsAdmin = e.IsAdmin
+                };
+                var paginatedList = await _repo.Players
+                    .Select(expression)
+                    .ToPaginatedListAsync(request.PageNumber, request.PageSize);
+                return paginatedList;
+            }
+        }
+    }
+}
