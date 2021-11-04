e80635526c4486cb72252a6c5fcd6c65443676c2
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-11-04 18:16:06 +0100

    styling forms

--- cypher-mobile-app/src/app/home/home.page.ts
+++ cypher-mobile-app/src/app/home/home.page.ts
@@ -12,53 +12,11 @@ import { LoginService } from '../Services/login.service';
 })
 export class HomePage {
 
-  userData: any = {};
-  displayName: any;
-  email: any;
-  familyName: any;
-  givenName: any;
-  userId: any;
-  imageUrl: any;
   isLoggedIn = false;
-  constructor(public googlePlus: GooglePlus, private router: Router, public nav: NavController, public loginservice: LoginService) { }
+  constructor(public googlePlus: GooglePlus, private router: Router,
+    public nav: NavController, public loginservice: LoginService) { }
 
   login() {
-    // const gplusUser = this.googlePlus.login({
-    //   webClientId: '646712186754-jlveohr7vqen6rl214hs3bsct3qf5ush.apps.googleusercontent.com',
-    //   offline: true,
-    //   scopes: 'profile email'
-    // })
-    //   .then(res => {
-    //     console.log(res);
-    //     this.displayName = res.displayName;
-    //     this.email = res.email;
-    //     this.familyName = res.familyName;
-    //     this.givenName = res.givenName;
-    //     this.userId = res.userId;
-    //     this.imageUrl = res.imageUrl;
-    //     this.isLoggedIn = true;
-    //     this.router.navigate(['game-screen']);
-    //   }, result => this.userData = 'Logged in')
-    //   .catch(err => console.error(err));
-    // ;
-    // return this.userData;
     this.loginservice.login();
   }
-
-  // logout() {
-  //   this.googlePlus.logout()
-  //     .then(res => {
-  //       console.log(res);
-  //       this.displayName = '';
-  //       this.email = '';
-  //       this.familyName = '';
-  //       this.givenName = '';
-  //       this.userId = '';
-  //       this.imageUrl = '';
-
-  //       this.isLoggedIn = false;
-  //     })
-  //     .catch(err => console.error(err));
-  // }
-
 }
