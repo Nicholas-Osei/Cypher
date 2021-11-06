fdab1f17f2ea975efb5c49d5ce4b58f93e03faf2
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-11-06 22:29:01 +0100

    Update UserCredentialController.cs

--- Cypher/Cypher.Api/Controllers/v1/UserCredentialController.cs
+++ Cypher/Cypher.Api/Controllers/v1/UserCredentialController.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Cypher.API.Controllers;
+using Cypher.Application.Features.UserCredentials.Commands.Create;
+using Cypher.Application.Features.UserCredentials.Queries.GetAllUserCredentials;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Cypher.Api.Controllers.v1
 {
-    public class UserCredentialController
+    public class UserCredentialController: BaseApiController<UserCredentialController>
     {
-        public UserCredentialController()
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int pageNumber, int pageSize)
         {
+            var players = await _mediator.Send(new GetAllUserCredentialsQuery(pageNumber, pageSize));
+            return Ok(players);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateUserCredentialsCommand command)
+        {
+            return Ok(await _mediator.Send(command));
         }
     }
 }
