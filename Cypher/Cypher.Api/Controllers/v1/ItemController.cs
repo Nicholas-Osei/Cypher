759d5a886dd05ef5f6c4ce4e9be567db94679a8f
yvesdj <yves.dejonghe@student.ap.be>
2021-11-18 10:14:54 +0100

    Created Item controller, queries/commands not yet implemented

---
+++ Cypher/Cypher.Api/Controllers/v1/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using Cypher.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cypher.Api.Controllers.v1
+{
+    public class ItemController : BaseApiController<ItemController>
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int pageNumber, int pageSize)
+        {
+            var items = await _mediator.Send(new GetAllItemsQuery(pageNumber, pageSize));
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var item = await _mediator.Send(new GetItemByIdQuery() { Id = id });
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateItemCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, UpdateItemCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeleteItemCommand { Id = id }));
+        }
+    }
+}
