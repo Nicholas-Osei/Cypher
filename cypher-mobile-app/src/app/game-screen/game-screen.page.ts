07bab7df1d6fd49562d48b3323e839f4e1107d4c
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-12-07 00:16:43 +0100

    make sidebar and link to player objects(friends,inventory,messages,enz...)
    make sidebar and link to player objects(friends,inventory,messages,enz...)
    A player can now see all his inventory , friends and messages (needs to be implemented in backend) via the sidebar

--- cypher-mobile-app/src/app/game-screen/game-screen.page.ts
+++ cypher-mobile-app/src/app/game-screen/game-screen.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../Services/login.service';
 import { Player, PlayerService } from '../Services/player.service';
 
+
 @Component({
   selector: 'app-game-screen',
   templateUrl: './game-screen.page.html',
@@ -11,6 +12,10 @@ export class GameScreenPage {
 
   players: Player;
   inventory: any;
+  showPage: any;
+  lobbies: any;
+  friends: any;
+  messages: any;
 
   constructor(public loginservice: LoginService, public speler: PlayerService) {
     this.speler.getAllPlayers().subscribe(p => {
@@ -25,7 +30,21 @@ export class GameScreenPage {
     this.players.data.forEach(m => {
       if (m.name === this.loginservice.displayName) {
         this.inventory = m.inventory.items;
+        this.messages = m.messages;
+        this.lobbies = m.playerLobbies;
       }
     });
+  }
+
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+  }
+
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+  }
+  renderPage(page: any) {
+    this.showPage = page;
+    this.closeNav();
   }
 }
