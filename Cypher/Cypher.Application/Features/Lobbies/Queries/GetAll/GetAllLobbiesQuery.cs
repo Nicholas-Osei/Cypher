0c023d23f7b6287b6580932f1c0469696832fff4
yvesdj <yves.dejonghe@student.ap.be>
2021-12-15 11:05:20 +0100

    Added GetAllQuery, needs testing

--- Cypher/Cypher.Application/Features/Lobbies/Queries/GetAll/GetAllLobbiesQuery.cs
+++ Cypher/Cypher.Application/Features/Lobbies/Queries/GetAll/GetAllLobbiesQuery.cs
@@ -1,10 +1,14 @@
 ﻿using AspNetCoreHero.Results;
+using Cypher.Application.Extensions;
 using Cypher.Application.Interfaces.Repositories;
+using Cypher.Domain.Entities.Cypher;
 using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cypher.Application.Features.Lobbies.Queries
@@ -25,6 +29,26 @@ namespace Cypher.Application.Features.Lobbies.Queries
         {
             private readonly ILobbyRepository _repo;
 
+            public GetAllLobbiesQueryHandler(ILobbyRepository repo)
+            {
+                _repo = repo;
+            }
+
+            public async Task<PaginatedResult<GetAllLobbiesResponse>> Handle(GetAllLobbiesQuery request, CancellationToken cancellationToken)
+            {
+                Expression<Func<Lobby, GetAllLobbiesResponse>> expression = e => new GetAllLobbiesResponse
+                {
+                    Id = e.Id,
+                    LobbyAdmin = e.LobbyAdmin,
+                    PlayerLobbies = e.PlayerLobbies
+                };
+
+                var paginatedList = await _repo.Lobbies
+                    .Select(expression)
+                    .ToPaginatedListAsync(request.PageNumber, request.PageSize);
+
+                return paginatedList;
+            }
         }
     }
 }
--- Cypher/Cypher.Application/Features/Lobbies/Queries/GetAll/GetAllLobbiesResponse.cs
+++ Cypher/Cypher.Application/Features/Lobbies/Queries/GetAll/GetAllLobbiesResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cypher.Domain.Entities.Cypher;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Cypher.Application.Features.Lobbies.Queries
 {
-    class GetAllLobbiesResponse
+    public class GetAllLobbiesResponse
     {
+        public int Id { get; set; }
+        public Player LobbyAdmin { get; set; }
+        public ICollection<PlayerLobby> PlayerLobbies { get; set; }
     }
 }
