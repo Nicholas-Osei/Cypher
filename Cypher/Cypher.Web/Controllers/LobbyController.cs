588b58c622bb5c85a8acae34ef1c0b589fed7852
yvesdj <yves.dejonghe@student.ap.be>
2021-12-24 10:35:43 +0100

    Scaffolding CTRL, added Cypher/Lobby to nav

---
+++ Cypher/Cypher.Web/Controllers/LobbyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cypher.Web.Controllers
+{
+    [Area("Cypher")]
+    public class LobbyController : BaseController<LobbyController>
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LoadAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
