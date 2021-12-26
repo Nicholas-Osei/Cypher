9cfcfadcad0f218ef4293dfc4ed61a7b837359f6
yvesdj <yves.dejonghe@student.ap.be>
2021-12-26 17:13:06 +0100

    Scafolded in right location, provided route to lobby, needs logic

---
+++ Cypher/Cypher.Web/Areas/Cypher/Controllers/LobbiesController.cs
@@ -0,0 +1,89 @@
+﻿using Cypher.Web.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cypher.Web.Areas.Cypher.Controllers
+{
+    [Area("Cypher")]
+    public class LobbiesController : BaseController<LobbiesController>
+    {
+        // GET: LobbiesController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LobbiesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LobbiesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LobbiesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LobbiesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LobbiesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LobbiesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LobbiesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
