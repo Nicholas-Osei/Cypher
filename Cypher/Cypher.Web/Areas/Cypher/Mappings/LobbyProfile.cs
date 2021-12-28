9e64e0c7d018ed2e729f0f4edddc7b1a19577c37
yvesdj <yves.dejonghe@student.ap.be>
2021-12-28 16:04:01 +0100

    Added profile for lobbyviewmodel

---
+++ Cypher/Cypher.Web/Areas/Cypher/Mappings/LobbyProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Cypher.Application.Features.Lobbies.CMDs.Create;
+using Cypher.Web.Areas.Cypher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cypher.Web.Areas.Cypher.Mappings
+{
+    internal class LobbyProfile : Profile
+    {
+        public LobbyProfile()
+        {
+            CreateMap<CreateLobbyCommand, LobbyViewModel>().ReverseMap();
+        }
+    }
+}
