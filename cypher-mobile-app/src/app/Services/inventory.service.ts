fb1b4889f045b1b47e54d4755e2b88c52686fc63
Arne Janssens <arne.janssens@student.ap.be>
2022-01-27 00:02:28 +0100

    Player can pickup items from small green circles

--- cypher-mobile-app/src/app/Services/inventory.service.ts
+++ cypher-mobile-app/src/app/Services/inventory.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LoginService } from '../Services/login.service';
-import { Player, ApiService } from '../Services/api.service';
+import { ApiService } from '../Services/api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,6 @@ import { Player, ApiService } from '../Services/api.service';
 
 export class InventoryService {
 
-  // players: Player;
-  // pf: any;
   inventory: any;
   inInventory = true;
   id: any;
@@ -18,25 +16,18 @@ export class InventoryService {
   itemLength: any;
   invInLocalStorage: any;
 
-  constructor(public api: ApiService, public loginservice: LoginService) 
-  {
+  constructor(public api: ApiService, public loginservice: LoginService) {
     this.refreshConnection();
   }
   
   refreshConnection(){
     this.api.getAllPlayers().subscribe(p => {
-      this.api.players = p; console
-        .log('Got all players');
+      this.api.players = p;
       this.api.players.data.forEach(m => {
         if (m.name === this.loginservice.displayName) {
-          console.log(m.name);
-          // this.id = m.id;
           this.getPlayerbyId(m.id);
           localStorage.setItem('playerId', JSON.stringify(m.id));
         }
-        // else if (this.speler.players.data.length === this.teller) {
-        //   console.log('ik ben hier');
-        // };
       });
     });
     
@@ -51,11 +42,6 @@ export class InventoryService {
         this.itemLength = localStorage.setItem('inventoryLength', this.playerbyId.data.inventory.items.length);
         this.invInLocalStorage = localStorage.setItem('inventoryItems', this.inventory);
         console.log(this.inventory);
-        
-        
-        // this.speler.inventory = this.playerbyId.data.inventory;
-        // this.speler.messages = m.messages;
-        // this.lobbies = m.playerLobbies;
       });
   }
   
@@ -67,10 +53,7 @@ export class InventoryService {
   addItemToInventory(form) {
     let newItems: any = [];
     const item = [];
-    console.log('clicked');
-    // this.playerbyId.data.inventory.forEach(m => {
-    // if (m.name === this.loginservice.displayName) {
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+
     for (let index = 0; index < this.playerbyId.data.inventory.items.length; index++) {
       item.push(
         {
@@ -79,29 +62,27 @@ export class InventoryService {
         },
       );
     }
+
     item.push(
       {
         name: form.value.naam,
         itemType: form.value.itemType
       }
     );
+
     newItems = {
       id: this.playerbyId.data.inventory.id,
       items: item
     };
+
     return this.api.updateInventoryItems(this.playerbyId.data.inventory.id, newItems).
       subscribe(l => { console.log('added'); form.value.naam = ''; form.value.itemType = ''; this.refreshConnection(); });
-    //}
-
-    // });
-
   }
+
   deleteItem(id: any) {
-    console.log(id);
     const todelete = confirm('Are you sure you want to delete');
     if (todelete) {
       this.api.deleteInventoryItem(id).subscribe(d => { console.log('Deleted'); this.refreshConnection(); });
     }
   }
-
 }
